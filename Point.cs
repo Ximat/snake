2a9c5151692f6ebfa23535d6be7c01c005012b6e
Roman Kovalchuk <rkovalchuk@it.ru>
2018-10-31 12:49:38 +0300

    add class Point

---
+++ Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sim;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,17 +4,14 @@ namespace snake
 {
     class Program
     {
-        static void Draw (int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
-        }
         static void Main(string[] args)
         {
-            int x1=5;
-            int y1=3;
-            char sim1='*';
-            Draw(x1,y1,sim1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sim = '*';
+            p1.Draw();
+
         }
     }
 }
