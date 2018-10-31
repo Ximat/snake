94b44b8f72efb513a603ab9f4b69ceb33e7afdcd
Roman Kovalchuk <rkovalchuk@it.ru>
2018-10-31 13:31:34 +0300

    added constractor

--- Point.cs
+++ Point.cs
@@ -6,6 +6,16 @@ namespace snake
         public int x;
         public int y;
         public char sim;
+
+        public Point (int _x, int _y, char _sim)
+        {
+            x = _x;
+            y = _y;
+            sim = _sim;
+
+        }
+
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
