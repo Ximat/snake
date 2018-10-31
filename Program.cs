4d63fb26c49bbfa978ceaa3b8c4cf5f4ff1766fb
Roman Kovalchuk <rkovalchuk@it.ru>
2018-10-31 12:20:13 +0300

    Snake the beginning

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ namespace snake
 {
     class Program
     {
+        static void Draw (int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1=5;
+            int y1=3;
+            char sim1='*';
+            Draw(x1,y1,sim1);
         }
     }
 }
