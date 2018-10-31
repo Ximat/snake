a84628d9ba7cb1a124616e88b3168652d70449ff
Roman Kovalchuk <rkovalchuk@it.ru>
2018-10-31 15:04:12 +0300

    Added class HorizontalLine

---
+++ HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace snake
+{
+    public class HorizontalLine
+    {
+        List<Point> pList;
+        public HorizontalLine(int xLeft, int xRight, int y, char sim)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++) 
+            {
+                Point p = new Point(x, y, sim);
+                pList.Add(p);
+            }
+
+        }
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,22 +11,8 @@ namespace snake
             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-
-
+            HorizontalLine line = new HorizontalLine(1, 10, 5, '+');
+            line.Drow();
         }
     }
 }
